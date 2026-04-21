0d413c59bb8de494e8d4ecaa86456fec89d488bc
Ambdulghaffar <elhaffarahamadi@gmail.com>
2026-04-22 00:49:38 +0100

    feat: first commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_demo/pages/appels.dart';
+import 'package:whatsapp_demo/pages/communities.dart';
+import 'package:whatsapp_demo/pages/discussions.dart';
+import 'package:whatsapp_demo/pages/actus.dart';
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        colorSchemeSeed: Colors.green,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final pages = [
+    const DiscussionsPage(),
+    const ActusPage(),
+    const CommunitiesPage(),
+    const AppelsPage(),
+  ];
+
+  int pageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[pageIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(top: BorderSide(color: Colors.grey, width: 0.2)),
+        ),
+        child: NavigationBar(
+          selectedIndex: pageIndex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              pageIndex = index;
+            });
+          },
+          backgroundColor: Colors.white,
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.message),
+              label: "Discussions",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.filter_tilt_shift),
+              label: "Actu",
+            ),
+            NavigationDestination(icon: Icon(Icons.group), label: "Communités"),
+            NavigationDestination(icon: Icon(Icons.phone), label: "Appels"),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/discussions.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DiscussionsPage extends StatelessWidget {
+  const DiscussionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(child: Text('Bienvenue dans la page Discussions')),
+    );
+  }
+}
